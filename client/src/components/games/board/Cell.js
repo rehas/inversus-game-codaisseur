58dfb29d6fea9a1604f0609dec9bd617d2d90377
isabellabrookes <hello@isabellabrookes.com>
2018-08-15 22:10:02 +0200

    fixed cells

--- client/src/components/games/board/Cell.js
+++ client/src/components/games/board/Cell.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import './Board.css'
 
-const renderCellVisuals = (coordinates_p1, coordinates_p2, beam_p1, beam_p2, cellIndex, rowIndex, playerNumber) => {
-
+const renderCellVisuals = (coordinates_p1, coordinates_p2, beam_p1, beam_p2, cellIndex, rowIndex) => {
   if (coordinates_p1.X === cellIndex && coordinates_p1.Y === rowIndex){
     return <div className={'board-cell-player1'}></div>
   } else if (coordinates_p2.X === cellIndex && coordinates_p2.Y === rowIndex) {
-    // console.log(beam_p1)
-    // console.log({X:cellIndex, Y:rowIndex})
     return <div className={'board-cell-player2'}></div>
   } else if (beam_p1.filter(beamCell=>{
         if(beamCell.X === cellIndex && beamCell.Y === rowIndex){
@@ -15,7 +12,14 @@ const renderCellVisuals = (coordinates_p1, coordinates_p2, beam_p1, beam_p2, cel
           }
         }
     ).length > 0) {
-     return <div className={`board-cell-laser-${playerNumber}`}></div>
+     return <div className={'board-cell-laser-1'}></div>
+  } else if (beam_p2.filter(beamCell=>{
+      if(beamCell.X === cellIndex && beamCell.Y === rowIndex){
+        return true
+      }
+    }
+  ).length > 0) {
+    return <div className={'board-cell-laser-2'}></div>
   } else return <div className={'board-cell-coordinates'}> </div>
 }
 
