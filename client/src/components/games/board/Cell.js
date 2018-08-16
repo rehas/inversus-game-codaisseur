05580a5a7205416c437e53939364c7d32dc9b705
isabellabrookes <hello@isabellabrookes.com>
2018-08-16 15:46:58 +0200

    clean up cell code

--- client/src/components/games/board/Cell.js
+++ client/src/components/games/board/Cell.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './Board.css'
 
 const renderCellVisuals = (coordinates_p1, coordinates_p2, beam_p1, beam_p2, cellIndex, rowIndex) => {
-  console.log(beam_p1.beamArray.some(beam => beam.X === cellIndex && beam.Y === rowIndex))
   if (coordinates_p1.X === cellIndex && coordinates_p1.Y === rowIndex){
     return <div className={`board-cell-player1`}></div>
   } else if (coordinates_p2.X === cellIndex && coordinates_p2.Y === rowIndex) {
