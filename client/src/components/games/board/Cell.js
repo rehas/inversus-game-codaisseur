168b25cc6196aeb551db87b603f12e0cfcfc650e
isabellabrookes <hello@isabellabrookes.com>
2018-08-16 10:51:07 +0200

    changed cell render method

--- client/src/components/games/board/Cell.js
+++ client/src/components/games/board/Cell.js
@@ -2,23 +2,14 @@ import React from 'react'
 import './Board.css'
 
 const renderCellVisuals = (coordinates_p1, coordinates_p2, beam_p1, beam_p2, cellIndex, rowIndex) => {
+  console.log(beam_p1.beamArray.some(beam => beam.X === cellIndex && beam.Y === rowIndex))
   if (coordinates_p1.X === cellIndex && coordinates_p1.Y === rowIndex){
     return <div className={`board-cell-player1`}></div>
   } else if (coordinates_p2.X === cellIndex && coordinates_p2.Y === rowIndex) {
     return <div className={'board-cell-player2'}></div>
-  } else if (beam_p1.beamArray.filter(beamCell => {
-        if (beamCell.X === cellIndex && beamCell.Y === rowIndex){
-            return true
-          }
-        }
-    ).length > 0) {
+  } else if (beam_p1.beamArray.some(beam => beam.X === cellIndex && beam.Y === rowIndex)) {
      return <div className={`board-cell-laser-1 beam-cell-laser-direction-${beam_p1.direction}`}></div>
-  } else if (beam_p2.beamArray.filter(beamCell=>{
-      if(beamCell.X === cellIndex && beamCell.Y === rowIndex){
-        return true
-      }
-    }
-  ).length > 0) {
+  } else if (beam_p2.beamArray.some(beam => beam.X === cellIndex && beam.Y === rowIndex)) {
     return <div className={`board-cell-laser-2 beam-cell-laser-direction-${beam_p2.direction}`}></div>
   } else return null
 }
