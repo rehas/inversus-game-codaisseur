aa6288dcf359797d717ae537f20f658bf81c1e21
isabellabrookes <hello@isabellabrookes.com>
2018-08-17 14:14:36 +0200

    staggered combo + master

--- client/src/components/games/board/BoardWrapper.js
+++ client/src/components/games/board/BoardWrapper.js
@@ -3,32 +3,54 @@ import Board from './Board'
 
 class BoardWrapper extends PureComponent {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      down: false,
       keysPressed: [],
+      keydownDirection: false,
+      keydownBeam: false,
+      keydownEvent: (e) => {this.state.keyInterval(e)},
+      keyupEvent: (e) => {this.state.onKeyUp(e)},
       keyInterval: (e) => {
-        if(this.state.down) return null
-        this.setState({down: true})
+        const beams = ['w','a','s','d']
+        const directions = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight']
         if (!this.state.keysPressed.includes(e.key)) this.setState({...this.state, keysPressed: [...this.state.keysPressed, e.key]})
-        setTimeout(() => {
-          this.props.onKeyDown(this.state.keysPressed, this.props.player, this.props.game)
-          this.setState({...this.state, down: false})
-        }, 150)
+        return this.state.keysPressed.forEach(key => {
+          if (directions.includes(key)) {
+            if (this.state.keydownDirection) return null
+            this.setState({...this.state, keydownDirection: true})
+            setTimeout(() => {
+              this.props.onKeyDown(key, this.props.player, this.props.game)
+              this.setState({...this.state, keydownDirection: false})
+            }, 100)
+          } else if (beams.includes(key)){
+            if (this.state.keydownBeam) return null
+            this.setState({...this.state, keydownBeam: true})
+            setTimeout(() => {
+              this.props.onKeyDown(key, this.props.player, this.props.game)
+              this.setState({...this.state, keydownBeam: false})
+            }, 1000)
+          }
+        })
       },
       onKeyUp: (e) => {
+        const beams = ['w','a','s','d']
+        const directions = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight']
         const newKeysPressed = [...this.state.keysPressed].filter(key => key !== e.key)
-        this.setState({down: false, keysPressed: newKeysPressed})
+        if(beams.includes(e.key)){
+          this.setState({...this.state, keydownBeam: false, keysPressed: newKeysPressed})
+        } else if(directions.includes(e.key)){
+          this.setState({...this.state, keydownDirection: false, keysPressed: newKeysPressed})
+        }
       }
     }
   }
   componentDidMount() {
-    document.addEventListener('keydown', (e) => {this.state.keyInterval(e)}, false)
-    document.addEventListener('keyup', (e) => {this.state.onKeyUp(e)}, false)
+    document.addEventListener('keydown', this.state.keydownEvent, false)
+    document.addEventListener('keyup', this.state.keyupEvent, false)
   }
   componentWillUnmount() {
-    document.removeEventListener('keydown', (e) => {this.state.keyInterval(e)}, false)
-    document.removeEventListener('keyup', (e) => {this.state.onKeyUp(e)}, false)
+    document.removeEventListener('keydown', this.state.keydownEvent, false)
+    document.removeEventListener('keyup', this.state.keyupEvent, false)
   }
 
   getBeamCells  (playerCoordinates, beamDirection)  {
